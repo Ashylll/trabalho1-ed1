cec5b38c951f97b68dd22d782a6b92a59ee6dbb6
Ashylll <ashleyfsaito@gmail.com>
2025-10-17 08:22:50 -0300

    Feature (.geo): implementa a leitura do arquivo de entrada .geo

---
+++ src/leitura.c
@@ -0,0 +1,111 @@
+#include "leitura.h"
+#include "saida.h"
+#include "chao.h"
+#include "forma.h"
+#include "circulo.h"
+#include "retangulo.h"
+#include "texto.h"
+#include "linha.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool ler_geo(const char *path_geo, CHAO chao, SAIDA saida){
+    FILE *fp = fopen(path_geo, "r");
+    if(!fp) return false;
+
+    char linha[1024], comando[8];
+
+    char fFamily[32] = "sans";
+    char fWeight[8] = "n";
+    int fSize = 12;
+
+    while (fgets(linha, sizeof linha, fp)){
+        if(sscanf(linha, "%7s", comando) != 1) continue;
+        if(comando[0] == '#') continue;
+
+        if (strcmp(comando, "c") == 0){
+            int i; 
+            double x, y, r;
+            char corb[32], corp[32];
+            CIRCULO c = NULL;
+
+            if(sscanf(linha, "%*s %d %lf %lf %lf %31s %31s", &i, &x, &y, &r, corb, corp) == 6){
+                c = criar_circulo(i, x, y, r, corb, corp);
+            } 
+
+            if (c){
+                FORMA f = criar_forma('c', c);
+                add_chao(chao, f);
+                add_forma_saida(saida, f);
+            }
+        }
+
+        else if (strcmp(comando, "r") == 0){
+            int i;
+            double x, y, w, h;
+            char corb[32], corp[32];
+            RETANGULO r = NULL;
+
+            if(sscanf(linha, "%*s %d %lf %lf %lf %lf %31s %31s", &i, &x, &y, &w, &h, corb, corp) == 7){
+                r = criar_retangulo(i, x, y, w, h, corb, corp);
+            }
+
+            if (r){
+                FORMA f = criar_forma('r', r);
+                add_chao(chao, f);
+                add_forma_saida(saida, f);
+            }
+        }
+
+        else if (strcmp(comando, "l") == 0){
+            int i;
+            double x1, y1, x2, y2;
+            char cor[32];
+            LINHA l = NULL;
+
+            if(sscanf(linha, "%*s %d %lf %lf %lf %lf %31s", &i, &x1, &y1, &x2, &y2, cor) == 6){
+                l = criar_linha(i, x1, y1, x2, y2, cor);
+            }
+
+            if (l){
+                FORMA f = criar_forma('l', l);
+                add_chao(chao, f);
+                add_forma_saida(saida, f);
+            }
+
+        }
+
+        else if(strcmp(comando, "t") == 0){
+            int i;
+            double x, y;
+            char corb[32], corp[32], ancora, txto[512];
+            TEXTO t = NULL;
+
+            if(sscanf(linha, "%*s %d %lf %lf %31s %31s %c %[^\n]", &i, &x, &y, corb, corp, &ancora, txto) >= 7){
+                t = criar_texto(i, x, y, corb, corp, ancora, txto);
+            }
+            
+            if (t){
+                (void)mudar_estilo(t, fFamily, fWeight, fSize);
+                FORMA f = criar_forma('t', t);
+                add_chao(chao, f);
+                add_forma_saida(saida, f);
+            }
+        }
+
+        else if (strcmp(comando, "ts") == 0){
+            if(sscanf(linha, "%*s %31s %7s %d", fFamily, fWeight, &fSize) == 3){
+                
+            }
+        }
+    }
+
+    fclose(fp);
+
+    return true;
+}
+
+
---
+++ src/leitura.h
@@ -0,0 +1,24 @@
+#ifndef LEITURA_H
+#define LEITURA_H
+
+#include "saida.h"
+#include "chao.h"
+
+#include <stdbool.h>
+
+/*
+    leitura.h
+    Módulo responsável pela leitura dos arquivos de entrada .geo e . qry
+
+*/
+
+/// @brief realiza a leitura do arquivo .geo e cria as formas correspondentes
+/// @param path_geo caminho para o arquivo .geo
+/// @param chao chão onde as formas são armazenadas 
+/// @param saida estrutura de saída para registrar as formas criadas
+/// @return true se o arquivo foi lido com sucesso, false se falhou ao abrir ou a ler
+bool ler_geo(const char *path_geo, CHAO chao, SAIDA saida);
+
+
+
+#endif
