c709e2d4140ce5aff7bae4c62d03bf9356b20302
Ashylll <ashleyfsaito@gmail.com>
2025-10-17 08:21:58 -0300

    Feature (chao): implementa o chão

---
+++ src/chao.c
@@ -0,0 +1,79 @@
+#include "chao.h"
+#include "forma.h"
+#include "fila.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct stChao{
+    FILA formas;
+    int n;
+
+} stChao;
+
+CHAO criar_chao(void){
+   stChao *chao = malloc(sizeof(*chao));
+   if(!chao) return NULL;
+   
+   chao->formas = criar_fila();
+   if(!chao->formas){
+    free(chao);
+    return NULL;
+   }
+
+   chao->n = 0;
+
+   return chao;
+}
+
+void destruir_chao(CHAO *c){
+    if(!c || !*c) return;
+    stChao *chao = (stChao*)*c;
+
+    void *aux;
+    while(rmv_fila(chao->formas, &aux));
+    destruir_fila(&chao->formas);
+
+    free(chao);
+    *c = NULL;
+}
+
+bool add_chao(CHAO c, FORMA f){
+    if(!c || !f) return false;
+    stChao *chao = (stChao*)c;
+    
+    if(!add_fila(chao->formas, f)) return false;
+
+    chao->n++;
+
+    return true;
+}
+
+bool rmv_chao(CHAO c, FORMA *out){
+    if(!c || !out) return false;
+    stChao *chao = (stChao*)c;
+
+    void *aux = NULL;
+    if(!rmv_fila(chao->formas, &aux)) return false;
+
+    chao->n--;
+
+    *out = (FORMA)aux;
+
+    return true;
+}
+
+bool empty_chao(CHAO c){
+    if (!c) return true;
+    stChao *chao = (stChao*)c;
+
+    return empty_fila(chao->formas);
+}
+
+int size_chao(CHAO c){
+    if (!c) return 0;
+    stChao *chao = (stChao*)c;
+
+    return chao->n;
+}
---
+++ src/chao.h
@@ -0,0 +1,49 @@
+#ifndef CHAO_H
+#define CHAO_H
+
+#include "forma.h"
+
+#include <stdbool.h>
+
+/*
+    chao.h
+    Módulo responsável pela criação e pelo gerenciamento do chão
+
+*/
+
+typedef void* CHAO;
+
+/// @brief cria o chão
+/// @return ponteiro para o chão
+CHAO criar_chao(void);
+
+/// @brief libera o chão
+/// @param c chão
+void destruir_chao(CHAO *c);
+
+/// @brief adiciona uma forma ao chão
+/// @param c chão
+/// @param f forma
+/// @return true se a operação foi bem sucedida, false se houve erro
+bool add_chao(CHAO c, FORMA f);
+
+/// @brief remove uma forma do chão
+/// @param c chão
+/// @param out handle para a forma a ser retirada
+/// @return true se a operação foi bem sucedida, false se houve erro
+bool rmv_chao(CHAO c, FORMA *out);
+
+/// @brief checa se não há formas no chão
+/// @param c chão
+/// @return true se estiver vazio, false se não
+bool empty_chao(CHAO c);
+
+/// @brief checa quantas formas estão no chão
+/// @param c chão
+/// @return número inteiro de formas
+int size_chao(CHAO c);
+
+
+
+
+#endif
