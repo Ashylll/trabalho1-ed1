8c86e152dea341156edea370d80c641da91966e5
Ashylll <ashleyfsaito@gmail.com>
2025-10-25 02:03:43 -0300

    Feature (leitura): implementa a leitura de .qry (falta a implementação do comando calc)

--- src/leitura.c
+++ src/leitura.c
@@ -1,11 +1,13 @@
 #include "leitura.h"
 #include "saida.h"
 #include "chao.h"
+#include "disparador.h"
 #include "forma.h"
 #include "circulo.h"
 #include "retangulo.h"
 #include "texto.h"
 #include "linha.h"
+#include "repo.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -108,4 +110,94 @@ bool ler_geo(const char *path_geo, CHAO chao, SAIDA saida){
     return true;
 }
 
+bool ler_qry(const char *path_qry, REPO repo, CHAO chao, SAIDA saida){
+    FILE *fp = fopen(path_qry, "r");
+    if(!fp) return false;
+
+    char linha[1024], comando [8];
+
+    while (fgets(linha, sizeof linha, fp)){
+        if (sscanf(linha, "%7s", comando) != 1) continue;
+        if (comando[0] == '#') continue;
+
+        if (strcmp(comando, "pd") == 0){
+            int l;
+            double x, y;
+            DISPARADOR d = NULL;
+
+            if (sscanf(linha, "%d %lf %lf", &l, &x, &y) == 3){
+                d = repo_assegurar_disparador(repo, l, x, y);
+                if (d) posicionar_disparador(d, x, y);
+            }
+        }
+
+        else if (strcmp(comando, "lc") == 0){
+            int c, n;
+            CARREGADOR c = NULL;
+            if (sscanf(linha, "%*s %d %d", &c, &n) == 2){
+                c = repo_assegurar_carregador(repo, c);
+                if (c) load_carregador(c, chao, n);
+            }
+        }
+
+        else if (strcmp(comando, "atch") == 0){
+            int d_id, cesq, dir;
+            DISPARADOR d;
+            CARREGADOR cesq;
+            CARREGADOR cdir;
+
+            if (sscanf(linha, "%*s %d %d %d", &d_id, &cesq, &cdir) == 3){
+                d = repo_get_disparador(repo, d_id);
+                if (d){
+                    cesq = repo_take_carregaodor(repo, cesq);
+                    cdir = repo_take_carregaodor(repo, cdir);
+                    if (cesq) encaixar_cesq(d, cesq);
+                    if (cdir) encaixar_cdir(d, cdir);
+                }  
+            }
+        }
+
+        else if (strcmp(comando, "shft") == 0){
+            int d_id, n;
+            char lado;
+            DISPARADOR d;
+
+            if (sscanf(linha, "%*s %d %c %d", &d_id, &lado, &n)){
+                d = repo_get_disparador(repo, d_id);
+                if (d) shift_disparador(d, lado, n);
+            }
+        }
+
+        else if (strcmp(comando, "dsp") == 0){
+            int d_id;
+            double dx, dy;
+            char modo = 'i';
+            DISPARADOR d;
+
+            if (sscanf(linha, "%*s %d %lf %lf  %c", &d_id, &dx, &dy, &modo) >= 3){
+                d = repo_get_disparador(repo, d_id);
+                if (d) disparo(d, dx, dy, modo, saida);
+            }
+        }
+
+        else if (strcmp(comando, "dsp") == 0){
+            int d_id;
+            char lado;
+            double dx, dy, ix, iy;
+            DISPARADOR d;
+
+            if (sscanf(linha, "%*s %d %c %lf %lf %lf %lf", &d_id, &lado, &dx, &dy, &ix, &iy) == 6){
+                d = repo_get_disparador(repo, d_id);
+                if (d) rajada(d, lado, dx, dx, ix, iy);
+            }
+        }
+
+        else if (strcmp(comando, "calc") == 0){
+
+        }
+    }
+
+    fclose(fp);
+    return true;
+}
 
--- src/leitura.h
+++ src/leitura.h
@@ -16,8 +16,16 @@
 /// @param path_geo caminho para o arquivo .geo
 /// @param chao chão onde as formas são armazenadas 
 /// @param saida estrutura de saída para registrar as formas criadas
-/// @return true se o arquivo foi lido com sucesso, false se falhou ao abrir ou a ler
+/// @return true se o arquivo foi lido com sucesso; false se falhou ao abrir ou a ler
 bool ler_geo(const char *path_geo, CHAO chao, SAIDA saida);
+
+/// @brief realiza a leitura do arquivo .qry e executa os comandos correspondentes
+/// @param path_qry caminho para o arquivo .qry
+/// @param repo repositório onde os disparadores e os carregadores são guardados
+/// @param chao chão onde as formas são armazenadas
+/// @param saida estrutura de saída para registrar formas criadas
+/// @return true se o arquivo foi lido com sucess; false se falhou ao abrir ou a ler
+bool ler_qry(const char *path_qry, REPO repo, CHAO chao, SAIDA saida);
 
 
 
