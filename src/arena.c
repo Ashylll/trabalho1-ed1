a32c17b25ed3becbddf99c3799e671983fde9e7f
Ashylll <ashleyfsaito@gmail.com>
2025-10-25 04:48:24 -0300

    Feature: implementa a arena

---
+++ src/arena.c
@@ -0,0 +1,48 @@
+#include "arena.h"
+#include <stdlib.h>
+
+typedef struct stArena {
+    FILA formas;
+} stArena;
+
+ARENA criar_arena(void){
+    stArena *a = malloc(sizeof *a);
+    if (!a) return NULL;
+
+    a->formas = criar_fila();
+
+    return a;
+}
+
+void destruir_arena(ARENA *a){
+    if (!a || !*a) return;
+    stArena *arena = (stArena*)*a;
+
+    destruir_fila(&arena->formas);
+    free(arena);
+    *a = NULL;
+}
+
+bool add_arena(ARENA a, FORMA f){
+    if (!a || !f) return false;
+    stArena *arena = (stArena*)a;
+
+    return add_fila(arena->formas, f);
+}
+
+FORMA rmv_arena(ARENA a){
+    if (!a) return NULL;
+    stArena *arena = (stArena*)a;
+
+    void *aux = NULL;
+    if (!rmv_fila(arena->formas, &aux)) return NULL;
+
+    return (FORMA)aux;
+}
+
+bool empty_arena(ARENA a){
+    if (!a) return true;
+    stArena *arena = (stArena*)a;
+    
+    return empty_fila(arena->formas);
+}
---
+++ src/arena.h
@@ -0,0 +1,39 @@
+#ifndef ARENA_H
+#define ARENA_H
+
+#include "fila.h"
+#include "forma.h"
+
+/*
+    arena.h
+    Módulo responsável pela criação e pelo gerenciamento da arena
+
+*/
+
+typedef void* ARENA;
+
+/// @brief cria a arena
+/// @return ponteiro para a arena
+ARENA criar_arena(void);
+
+/// @brief libera a arena
+/// @param a arena
+void destruir_arena(ARENA *a);
+
+/// @brief adiciona uma forma à arena
+/// @param a arena
+/// @param f forma
+/// @return true se a operação foi bem sucedida, false se houve erro
+bool add_arena(ARENA a, FORMA f);
+
+/// @brief remove uma forma da arena
+/// @param a arena
+/// @return true se a operação foi bem sucedida, false se houve erro 
+FORMA rmv_arena(ARENA a);
+
+/// @brief checa se não há formas na arena
+/// @param a arena
+/// @return true se estiver vazio, false se não
+bool empty_arena(ARENA a);
+
+#endif
